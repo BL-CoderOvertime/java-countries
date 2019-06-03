630eed7633ca2ee5fe377992092014fa127b150f
BL-CoderOvertime <brandonlively@live.com>
2019-06-03 15:48:59 -0400

    Intial Commit

---
+++ src/main/kotlin/com/example/kotlincountries/KotlinCountriesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlincountries
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCountriesApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCountriesApplication>(*args)
+}
